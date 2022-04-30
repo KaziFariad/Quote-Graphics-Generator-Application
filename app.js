9f3ed457febf1979428e4ae86267547aec355646
Kazi Fariad <4kfislam@gmail.com>
2022-04-30 08:26:00 +0530

    added quotesDb

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello world!")
+import { readFile, writeFile } from 'fs';
+
+readFile('./quotesDb.json', 'utf8', (err, response) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	const data = JSON.parse(response);
+	console.log(data.slice(0, 3), data.slice(-2));
+});
