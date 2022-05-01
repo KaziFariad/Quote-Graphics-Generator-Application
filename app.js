2f2d8ae88f12bf549d4ad373f1008d1be9cded84
Kazi Fariad <4kfislam@gmail.com>
2022-05-01 09:48:06 +0530

    implement saveImage fn

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import 'dotenv/config';
 import handleStorage from './storage.js';
 import { getQuote } from './quote.js';
-import { getImageLink } from './image.js';
+import { getImageLink, saveImage } from './image.js';
 
 const id = handleStorage(process.env.CURRENT_VALUE);
 const quote = await getQuote(process.env.QUOTES_DB, id);
-getImageLink();
+const imageLink = await getImageLink(process.env.API_KEY, id);
+const outputPath = `./images/image${id}.png`;
+saveImage(imageLink, outputPath);
 
-// const saveImage = () => {};
 // const editImage = () => {};
--- image.js
+++ image.js
@@ -1,3 +1,35 @@
-export const getImageLink = () => {
-  console.log("")
+import { createClient } from 'pexels';
+import { createWriteStream } from 'fs';
+import axios from 'axios';
+import * as stream from 'stream';
+import { promisify } from 'util';
+
+export const getImageLink = async (key, id) => {
+	const client = createClient(key);
+	const query = 'Nature';
+	const page = id;
+	const orientation = 'portrait';
+
+	const photos = await client.photos.search({
+		orientation,
+		page,
+		per_page: 1,
+		query,
+	});
+
+	const imageLink = photos.photos[0].src[orientation];
+	return imageLink;
+};
+
+const finished = promisify(stream.finished);
+export const saveImage = async (imageURL, outputLocationPath) => {
+	const writer = createWriteStream(outputLocationPath);
+	return axios({
+		method: 'get',
+		url: imageURL,
+		responseType: 'stream',
+	}).then((response) => {
+		response.data.pipe(writer);
+		return finished(writer);
+	});
 };
