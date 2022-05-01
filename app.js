31e09d342261c61bf38f86094cfa20a709c19fb6
Kazi Fariad <4kfislam@gmail.com>
2022-05-01 11:49:53 +0530

    edit image

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import 'dotenv/config';
 import handleStorage from './storage.js';
 import { getQuote } from './quote.js';
-import { getImageLink, saveImage } from './image.js';
+import { getImageLink, saveImage, editImage } from './image.js';
 
 const id = handleStorage(process.env.CURRENT_VALUE);
 const quote = await getQuote(process.env.QUOTES_DB, id);
 const imageLink = await getImageLink(process.env.API_KEY, id);
 const outputPath = `./images/image${id}.png`;
-saveImage(imageLink, outputPath);
-
-// const editImage = () => {};
+await saveImage(imageLink, outputPath);
+await editImage(outputPath, quote[0].text);
--- image.js
+++ image.js
@@ -3,6 +3,7 @@ import { createWriteStream } from 'fs';
 import axios from 'axios';
 import * as stream from 'stream';
 import { promisify } from 'util';
+import Jimp from 'jimp';
 
 export const getImageLink = async (key, id) => {
 	const client = createClient(key);
@@ -22,6 +23,7 @@ export const getImageLink = async (key, id) => {
 };
 
 const finished = promisify(stream.finished);
+
 export const saveImage = async (imageURL, outputLocationPath) => {
 	const writer = createWriteStream(outputLocationPath);
 	return axios({
@@ -32,4 +34,33 @@ export const saveImage = async (imageURL, outputLocationPath) => {
 		response.data.pipe(writer);
 		return finished(writer);
 	});
+};
+
+export const editImage = async (filePath, text) => {
+	console.log('editing image in', filePath);
+	const image = await Jimp.read(filePath);
+	image.resize(1080, Jimp.AUTO);
+	const font = await Jimp.loadFont('./fonts/BebasNeue.fnt');
+	const [x, y] = [0, 0];
+	const content = text;
+  const [maxWidth, maxHeight] = [700, 700];
+  
+	const filePathTempPos = filePath.indexOf('.', 1);
+	const newFilePath =
+		filePath.slice(0, filePathTempPos) +
+		'updated' +
+		filePath.slice(filePathTempPos);
+	image.print(
+		font,
+		x,
+		y,
+		{
+			text: content,
+			alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+			alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+		},
+		maxWidth,
+		maxHeight
+	);
+	image.write(newFilePath);
 };
