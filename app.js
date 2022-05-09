8208b8a9419d7297b0191a43c37dad8ff570cc6b
Kazi Fariad <4kfislam@gmail.com>
2022-05-09 16:23:21 +0530

    Refactor

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ import handleStorage from './storage.js';
 import { getQuote } from './quote.js';
 import { getImageLink, saveImage, editImage } from './image.js';
 
-for (let i = 0; i < 10; i++) {
-  const id = handleStorage('counter.json');
-  const quote = await getQuote('quotesDb.json', id);
-  const imageLink = await getImageLink(process.env.API_KEY, id);
-  const outputPath = `./images/image${id}.png`;
-  await saveImage(imageLink, outputPath);
-  await editImage(outputPath, quote[0].text, quote[0].author);
-}
+const id = handleStorage('counter.json');
+const quote = await getQuote('quotesDb.json', id);
+const imageLink = await getImageLink(process.env.API_KEY, id);
+const outputPath = `./images/image${id}.png`;
+await saveImage(imageLink, outputPath);
+await editImage(outputPath, quote[0].text, quote[0].author);
--- utils.js
+++ utils.js
@@ -1,17 +1,42 @@
-export const analyzeColour = (hex) => {
-  if (hex.indexOf('#') === 0) {
-    hex = hex.slice(1);
-  }
-  // convert 3-digit hex to 6-digits.
-  if (hex.length === 3) {
-    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-  }
-  if (hex.length !== 6) {
-    throw new Error('Invalid HEX color.');
-  }
-  var r = parseInt(hex.slice(0, 2), 16),
-    g = parseInt(hex.slice(2, 4), 16),
-    b = parseInt(hex.slice(4, 6), 16);
+import Jimp from 'jimp';
 
-  return r * 0.299 + g * 0.587 + b * 0.114 > 186;
+export const analyzeColour = (hex) => {
+	if (hex.indexOf('#') === 0) {
+		hex = hex.slice(1);
+	}
+	// convert 3-digit hex to 6-digits.
+	if (hex.length === 3) {
+		hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+	}
+	if (hex.length !== 6) {
+		throw new Error('Invalid HEX color.');
+	}
+	var r = parseInt(hex.slice(0, 2), 16),
+		g = parseInt(hex.slice(2, 4), 16),
+		b = parseInt(hex.slice(4, 6), 16);
+
+	return r * 0.299 + g * 0.587 + b * 0.114 > 186;
+};
+
+export const displayTextOnImage = (
+	image,
+	font,
+	x,
+	y,
+	textToDisplay,
+	maxWidth,
+	maxHeight
+) => {
+	image.print(
+		font,
+		x,
+		y,
+		{
+			text: textToDisplay,
+			alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+			alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+		},
+		maxWidth,
+		maxHeight
+	);
 };
