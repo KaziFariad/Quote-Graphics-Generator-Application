70cd4b1e8e47487d37123bc0b74d15d1f1c76a2e
Kazi Fariadul Islam <53252317+KaziFariad@users.noreply.github.com>
2022-05-09 05:08:16 -0500

    Add author name

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ import handleStorage from './storage.js';
 import { getQuote } from './quote.js';
 import { getImageLink, saveImage, editImage } from './image.js';
 
-const id = handleStorage(process.env.CURRENT_VALUE);
-const quote = await getQuote(process.env.QUOTES_DB, id);
-const imageLink = await getImageLink(process.env.API_KEY, id);
-const outputPath = `./images/image${id}.png`;
-await saveImage(imageLink, outputPath);
-await editImage(outputPath, quote[0].text);
+for (let i = 0; i < 10; i++) {
+  const id = handleStorage('counter.json');
+  const quote = await getQuote('quotesDb.json', id);
+  const imageLink = await getImageLink(process.env.API_KEY, id);
+  const outputPath = `./images/image${id}.png`;
+  await saveImage(imageLink, outputPath);
+  await editImage(outputPath, quote[0].text, quote[0].author);
+}
--- image.js
+++ image.js
@@ -1,66 +1,134 @@
 import { createClient } from 'pexels';
 import { createWriteStream } from 'fs';
-import axios from 'axios';
 import * as stream from 'stream';
 import { promisify } from 'util';
 import Jimp from 'jimp';
+import axios from 'axios';
+import { analyzeColour } from './utils.js';
+
+let prominentColour;
 
 export const getImageLink = async (key, id) => {
-	const client = createClient(key);
-	const query = 'Nature';
-	const page = id;
-	const orientation = 'portrait';
+  const client = createClient(key);
+  const query = 'Nature';
+  const page = id;
+  const orientation = 'portrait';
 
-	const photos = await client.photos.search({
-		orientation,
-		page,
-		per_page: 1,
-		query,
-	});
+  const photos = await client.photos.search({
+    orientation,
+    page,
+    per_page: 1,
+    query,
+  });
 
-	const imageLink = photos.photos[0].src[orientation];
-	return imageLink;
+  prominentColour = photos.photos[0].avg_color;
+  const imageLink = photos.photos[0].src[orientation];
+  return imageLink;
 };
 
 const finished = promisify(stream.finished);
 
 export const saveImage = async (imageURL, outputLocationPath) => {
-	const writer = createWriteStream(outputLocationPath);
-	return axios({
-		method: 'get',
-		url: imageURL,
-		responseType: 'stream',
-	}).then((response) => {
-		response.data.pipe(writer);
-		return finished(writer);
-	});
+  const writer = createWriteStream(outputLocationPath);
+  return axios({
+    method: 'get',
+    url: imageURL,
+    responseType: 'stream',
+  }).then((response) => {
+    response.data.pipe(writer);
+    return finished(writer);
+  });
 };
 
-export const editImage = async (filePath, text) => {
-	console.log('editing image in', filePath);
-	const image = await Jimp.read(filePath);
-	image.resize(1080, Jimp.AUTO);
-	const font = await Jimp.loadFont('./fonts/BebasNeue.fnt');
-	const [x, y] = [0, 0];
-	const content = text;
-  const [maxWidth, maxHeight] = [700, 700];
-  
-	const filePathTempPos = filePath.indexOf('.', 1);
-	const newFilePath =
-		filePath.slice(0, filePathTempPos) +
-		'updated' +
-		filePath.slice(filePathTempPos);
-	image.print(
-		font,
-		x,
-		y,
-		{
-			text: content,
-			alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-			alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
-		},
-		maxWidth,
-		maxHeight
-	);
-	image.write(newFilePath);
+export const editImage = async (filePath, quote, author) => {
+  console.log('editing image in', filePath);
+  const image = await Jimp.read(filePath);
+  image.resize(1080, Jimp.AUTO);
+  const lightfont = await Jimp.loadFont('./fonts/BebasNeue.fnt');
+  const darkFont = await Jimp.loadFont('./fonts/BebasNeueDark.fnt');
+  const lightFontForAuthorName = await Jimp.loadFont(
+    './fonts/BebasNeueNameLight.fnt'
+  );
+  const darkFontForAuthorName = await Jimp.loadFont(
+    './fonts/BebasNeueNameDark.fnt'
+  );
+  let font, fontInverse, mode, authorNameFont, authorNameFontInverse;
+
+  if (analyzeColour(prominentColour)) {
+    font = darkFont;
+    fontInverse = lightfont;
+    mode = 'brighten';
+    authorNameFont = darkFontForAuthorName;
+    authorNameFontInverse = lightFontForAuthorName;
+  } else {
+    font = lightfont;
+    fontInverse = darkFont;
+    mode = 'darken';
+    authorNameFont = lightFontForAuthorName;
+    authorNameFontInverse = darkFontForAuthorName;
+  }
+  const [x, y] = [200, 200];
+  const [maxWidth, maxHeight] = [700, 1000];
+  const offsetForShadow = 3;
+  const offsetForAuthorName = 400;
+  const filePathTempPos = filePath.indexOf('.', 1);
+  const newFilePath =
+    filePath.slice(0, filePathTempPos) +
+    'updated' +
+    filePath.slice(filePathTempPos);
+  image.color([{ apply: mode, params: [10] }]);
+  image.print(
+    fontInverse,
+    x,
+    y,
+    {
+      text: quote,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+    },
+    maxWidth,
+    maxHeight,
+    (err, image, { a, b }) => {
+      image.print(
+        font,
+        x - offsetForShadow,
+        y - offsetForShadow,
+        {
+          text: quote,
+          alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+          alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+        },
+        maxWidth,
+        maxHeight
+      );
+    }
+  );
+  image.print(
+    authorNameFontInverse,
+    x,
+    maxHeight - offsetForAuthorName,
+    {
+      text: '-- ' + author,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+    },
+    maxWidth,
+    maxHeight,
+    (err, image, { a, b }) => {
+      image.print(
+        authorNameFont,
+        x - offsetForShadow,
+        maxHeight - offsetForAuthorName - offsetForShadow,
+        {
+          text: '-- ' + author,
+          alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+          alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+        },
+        maxWidth,
+        maxHeight
+      );
+    }
+  );
+
+  image.write(newFilePath);
 };
