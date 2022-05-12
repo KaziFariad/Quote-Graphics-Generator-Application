77f5c0cf48201addf7efe3fdd75358acaec593db
Kazi Fariad <4kfislam@gmail.com>
2022-05-12 14:51:20 +0530

    Add whatsapp-web.js

--- app.js
+++ app.js
@@ -2,10 +2,57 @@ import 'dotenv/config';
 import handleStorage from './storage.js';
 import { getQuote } from './quote.js';
 import { getImageLink, saveImage, editImage } from './image.js';
+import wwebpkgs from 'whatsapp-web.js';
+const { Client, LocalAuth, MessageMedia } = wwebpkgs;
+import qrcpkgs from 'qrcode-terminal';
+const { qrcode } = qrcpkgs;
 
-const id = handleStorage('counter.json');
-const quote = await getQuote('quotesDb.json', id);
-const imageLink = await getImageLink(process.env.API_KEY, id);
-const outputPath = `./images/image${id}.png`;
-await saveImage(imageLink, outputPath);
-await editImage(outputPath, quote[0].text, quote[0].author);
+const imageMainFn = async () => {
+	const id = handleStorage('counter.json');
+	const quote = await getQuote('quotesDb.json', id);
+	const imageLink = await getImageLink(process.env.API_KEY, id);
+	const outputPath = `./images/image${id}.png`;
+	await saveImage(imageLink, outputPath);
+	const savedImagePath = await editImage(
+		outputPath,
+		quote[0].text,
+		quote[0].author
+	);
+	return [savedImagePath, quote];
+};
+
+const client = new Client({
+	authStrategy: new LocalAuth(),
+});
+
+client.initialize();
+
+client.on('qr', (qr) => {
+	qrcode.generate(qr, { small: true });
+});
+
+client.on('authenticated', () => {
+	console.log('AUTHENTICATED');
+});
+
+client.on('auth_failure', (msg) => {
+	// Fired if session restore was unsuccessful
+	console.error('AUTHENTICATION FAILURE', msg);
+});
+
+client.on('ready', async () => {
+	console.log('READY');
+});
+
+client.on('message', async (msg) => {
+	if (msg.body == '!pic') {
+		let [path, quote] = await imageMainFn();
+		msg.reply('sending a quote graphic picture...');
+		let chat = await msg.getChat();
+		// await new Promise((r) => setTimeout(r, 10000));
+		const media = MessageMedia.fromFilePath(path);
+		chat.sendMessage(media, { caption: quote[0].text });
+	}
+});
+
+// client.destroy();
--- image.js
+++ image.js
@@ -106,4 +106,5 @@ export const editImage = async (filePath, quote, author) => {
 	);
 
 	image.write(newFilePath);
+	return newFilePath;
 };
